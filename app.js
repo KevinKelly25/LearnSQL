348e33615b3aac487384e0d5a0b54883fdd6abfa
DESKTOP-CCFFGEO\Kevin <kelly263@connect.wcsu.edu>
2018-06-28 09:15:38 -0400

    Added needed packages

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const path = require('path');
 const favicon = require('serve-favicon');
+const flash = require('connect-flash');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
+const request = require('request');
+const session = require('express-session');
 const routes = require('./server/routes/index');
-
+const path = require('path');
 const app = express();
 
 
