c86f6fff1c57cd44ea343d186b4c1e8def9e0e44
DESKTOP-CCFFGEO\Kevin <kelly263@connect.wcsu.edu>
2018-07-01 15:08:45 -0400

    Update-Pg
    Updated pg module and the related code

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,25 +1,16 @@
 angular.module('nodeQuestion', [])
 .controller('Question', ($scope, $http) => {
-  $scope.formData = {};
-  $scope.answerData = {};
-  $scope.correctData = {};
-  $scope.statement = {};
-  $scope.statementInHTML = {};
-  $scope.answer = '';
-  $scope.backgroundColor = 'black';
   $scope.answerQuestion = () => {
     $scope.statement = {"text":$scope.statementInHTML};
     $http.post('/api/v1/WCDB', $scope.formData)//The submitted answer
     .success((data1) => {
       delete $scope.formData;
       $scope.answerData = data1;
-      console.log(data1);
       $http.post('/api/v1/WCDB', $scope.statement)//the correct answer
       .success((data2) => {
         delete $scope.statement;
         $scope.correctData = data2;
         $scope.answer = $scope.answerData;
-        console.log(data2);
         if (angular.equals($scope.answerData,$scope.correctData)) {//if the correct answer and submitted answer match
           $scope.answer = 'Your Answer Is Correct';
           $scope.backgroundColor = 'green';
@@ -34,7 +25,6 @@ angular.module('nodeQuestion', [])
       })
       //if the correct answer has an error processing
       .error((error) => {
-        console.log('Error: ' + error);
         $scope.answer = "Your Answer is Incorrect";
         $scope.backgroundColor = 'red';
         delete $scope.statement;
@@ -45,7 +35,6 @@ angular.module('nodeQuestion', [])
     })
     //if the submitted answer conneciton fails
     .error((error) => {
-      console.log('Error: ' + error);
       $scope.answer = "Your Answer is Incorrect";
       $scope.backgroundColor = 'red';
       delete $scope.statement;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,44 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const pg = require('pg');
+const { Pool } = require('pg');
 const path = require('path');
-const connectionString = 'postgres://postgres:password@localhost:5432/WCDB';
+const connectionString = 'postgresql://postgres:password@localhost:5432/WCDB';
 
 router.get('/', (req, res, next) => {
   res.sendFile(path.join(
     __dirname, '..', '..', 'client', 'views', 'index.html'));
 });
 
-router.get('/api/v1/WCDB', (req, res, next) => {
-  const results = [];
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Select Data
-    const query = client.query('SELECT * FROM Users ORDER BY id ASC;');
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
-  });
-});
-
 router.post('/api/v1/WCDB', (req, res, next) => {
   const results = [];
+  const pool = new Pool({
+    connectionString: connectionString,
+  })
   // Grab data from http request
   const data = {text: req.body.text, complete: false};
   // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
+  pool.connect((err, client, done) => {
     // Handle connection errors
     if(err) {
       done();
@@ -46,23 +25,19 @@ router.post('/api/v1/WCDB', (req, res, next) => {
       return res.status(500).json({success: false, data: err});
     }
     // SQL Query > Select Data
-    const query = client.query(data.text, (err, res2) => {
-      //if the sent in answer is not a valid SQL statement
+    var query = client.query(data.text, (err, response) => {
+      done()
       if (err) {
-        done();
         return res.status(500).json({success: false, data: err});
+      } else {
+        response.rows.forEach(row =>{
+          results.push(row)
+        })
+        var JSONobject = JSON.stringify(results);
+        pool.end()
+        return res.json(results);
       }
-    });
-
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
+    })
   });
 });
 
