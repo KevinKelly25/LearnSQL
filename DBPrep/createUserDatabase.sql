2a87647b37509b693f117b27346b5fee67611774
DESKTOP-CCFFGEO\Kevin <kelly263@connect.wcsu.edu>
2018-07-07 11:58:25 -0400

    Add basic login modal

--- DBPrep/createUserDatabase.sql
+++ DBPrep/createUserDatabase.sql
@@ -1,22 +1,22 @@
 Create Table IF NOT EXISTS Users (
-  UserID			   VARCHAR(30) NOT NULL PRIMARY KEY,
-  FullName       VARCHAR(60) NOT NULL,
-  Password       VARCHAR(60) NOT NULL,
-  Email		       VARCHAR(60) NOT NULL,
-  DateJoined	   DATE DEFAULT CURRENT_DATE,
-  isTeacher      BOOLEAN DEFAULT FALSE
-  isAdmin      BOOLEAN DEFAULT FALSE
+  UserID                  VARCHAR(30) NOT NULL PRIMARY KEY,
+  FullName                VARCHAR(60) NOT NULL,
+  Password                VARCHAR(60) NOT NULL,
+  Email                   VARCHAR(60) NOT NULL,
+  DateJoined              DATE DEFAULT CURRENT_DATE,
+  isTeacher               BOOLEAN DEFAULT FALSE
+  isAdmin                 BOOLEAN DEFAULT FALSE
 );
 
 Create Table IF NOT EXISTS Class (
-  ClassID			   VARCHAR(30) NOT NULL PRIMARY KEY,
-  ClassName      VARCHAR(60),
-  Password       VARCHAR(60)
+  ClassID                 VARCHAR(30) NOT NULL PRIMARY KEY,
+  ClassName               VARCHAR(60),
+  Password                VARCHAR(60)
 );
 
 Create Table IF NOT EXISTS Attends (
-  ClassID			   VARCHAR(30) NOT NULL REFERENCES Class,
-  UserID			   VARCHAR(30) NOT NULL PRIMARY KEY,
-  isTeacher      BOOLEAN,
+  ClassID                 VARCHAR(30) NOT NULL REFERENCES Class,
+  UserID                  VARCHAR(30) NOT NULL PRIMARY KEY,
+  isTeacher               BOOLEAN,
   PRIMARY KEY (ClassID, UserID)
 );
