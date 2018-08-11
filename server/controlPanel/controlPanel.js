39a59d3daf7aeceab554d984a64317dceaf2894e
DESKTOP-CCFFGEO\Kevin <kelly263@connect.wcsu.edu>
2018-08-11 17:25:19 -0400

    Update chained Queries to task
    Removed multiple functions and consolidated them into a single task

--- server/controlPanel/controlPanel.js
+++ server/controlPanel/controlPanel.js
@@ -4,53 +4,36 @@ var uniqid = require('uniqid');
 /**
  * This funciton creates a class database using a ClassDB template Database
  */
- // TODO: this should be a task of multiple queries
 function createClass(req, res) {
 	return handleErrors(req)
 	.then(() => {
 		var classid = req.body.name + '_' + uniqid(); //guarantee uniqueness
-		db.none('CREATE DATABASE $1~ WITH TEMPLATE classdb_template OWNER classdb', classid)
-		.then(() => {
-				console.log('getting to 1');
-				addClassToDB(classid, req.body.name, req.body.password);
-				addInstructorToDB(req.user.username, classname);
-				return res.status(200).json('Class Database Created Successfully');
-		})
-		.catch(error => {
-			console.log('getting to 2');
-			console.log(error);
-			res.status(400).json({status: error});
-		})
-	})
-}
-
-function addInstructorToDB(username, classid) {
-	db.none('INSERT INTO attends(username, classid, isteacher) VALUES(${name}, ${class}, ${isTeacher})'
-	, {
-		name: username,
-		class: classid,
-		isTeacher: true
-	})
-	.then(() => {
-			return res.status(200).json('Instructor Added Successfully');
-	})
-	.catch((error) => {
-			res.status(400).json({status: 'Could not add instructor to DB'});
-	})
-}
-
-function addClassToDB(classname, classname, password) {
-	db.none('INSERT INTO class(classid, classname, password) VALUES(${id}, ${name}, ${password}) '
-	, {
-		id: classid,
-		name: classname,
-		password: password
-	})
-	.then(() => {
-			return res.status(200).json('Class Added Successfully');
-	})
-	.catch((error) => {
-			res.status(400).json({status: 'Could not add Class to DB'});
+		db.task(t => {
+        // this.ctx = task config + state context;
+        return t.none('CREATE DATABASE $1~ WITH TEMPLATE classdb_template OWNER classdb', classid)
+            .then(() => {
+							return t.none('INSERT INTO class(classid, classname, password) VALUES(${id}, ${name}, ${password}) '
+						, {
+							id: classid,
+							name: req.body.name,
+							password: req.body.password
+						})
+						.then(() => {
+							return t.none('INSERT INTO attends(username, classid, isteacher) VALUES(${name}, ${class}, ${isTeacher})'
+							, {
+								name: req.user.username,
+								class: classid,
+								isTeacher: true
+								});
+							});
+					});
+    })
+    .then(events => {
+        return res.status(200).json('Class Database Created Successfully');
+    })
+    .catch(error => {
+        res.status(400).json({status: error});
+    });
 	})
 }
 
