3585067095cf81186a86b0ddf17af43c3492cb12
michael torres <mittorres2@gmail.com>
2018-11-29 01:22:32 -0500

    fixed concatenation error

--- src/webapp/server/controlPanel/workshopControlPanel.js
+++ src/webapp/server/controlPanel/workshopControlPanel.js
@@ -20,10 +20,10 @@ const logger = require('../logs/winston.js');
 function getClasses(req, res) {
   return new Promise((resolve, reject) => {
     ldb.any(
-      'SELECT Attends.ClassID, ClassName, Section, Times, Days, StartDate, EndDate, UserData_t.isTeacher'
-      + 'FROM LearnSQL.Attends INNER JOIN LearnSQL.Class_t ON Attends.ClassID = Class_t.ClassID'
-      + 'INNER JOIN LearnSQL.UserData_t on Attends.username = UserData_t.username'
-      + 'WHERE UserData_t.username = $1', [req.user.username],
+      'SELECT Attends.ClassID, ClassName, Section, Times, Days, StartDate, EndDate, UserData_t.isTeacher '
+    + 'FROM LearnSQL.Attends INNER JOIN LearnSQL.Class_t ON Attends.ClassID = Class_t.ClassID '
+    + 'INNER JOIN LearnSQL.UserData_t ON Attends.username = UserData_t.username '
+    + 'WHERE UserData_t.username = $1', [req.user.username],
     )
       .then((result) => {
         resolve();
